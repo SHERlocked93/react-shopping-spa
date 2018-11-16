eae377bfd88c991403c8c716d9b8cda7aef2934f
SHERlocked93 <443033099@qq.com>
2018-11-16 10:20:39 +0800

    feat 环境搭建

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.scss'
+
+ReactDOM.render(
+  < h1> Hello, world ~! <i className="fa fa-address-card-o" aria-hidden="true"/></h1>,
+  document.getElementById('app')
+)
+
