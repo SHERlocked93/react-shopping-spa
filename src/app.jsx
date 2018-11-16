878f2aabd0152da68035a83b074e0cb7c3068e49
SHERlocked93 <443033099@qq.com>
2018-11-16 16:38:37 +0800

    feat react-router通配

--- src/app.jsx
+++ src/app.jsx
@@ -16,6 +16,7 @@ class A extends Component {
         <div>Component A</div>
         <Switch>
           <Route path={`${this.props.match.path}`} render={route => <div>当前组件是不带参数的A</div>} exact/>
+          <Route path={`${this.props.match.path}/sub`} render={route => <Sub>进入Sub组件</Sub>} exact/>
           <Route path={`${this.props.match.path}/:id`} render={route => <div>当前组件是带参数的A：{route.match.params.id}</div>}/>
         </Switch>
       </div>
@@ -31,6 +32,18 @@ class B extends Component {
   render() {
     return (
       <div>Component B</div>
+    )
+  }
+}
+
+class Sub extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return (
+      <div>当前是sub组件</div>
     )
   }
 }
@@ -53,6 +66,8 @@ ReactDOM.render(
     <Wrapper> Hello, world ~! <i className="fa fa-address-card-o" aria-hidden="true"/>
       <br/>
       <Link to='/a'>组件A</Link>
+      <br/>
+      <Link to='/a/sub'>组件A 进入/a/sub</Link>
       <br/>
       <Link to='/a/1231231'>组件A 带参数</Link>
       <br/>
